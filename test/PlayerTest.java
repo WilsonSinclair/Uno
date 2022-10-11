e3c967acb83dd0323273f30a799c1ec895cfa67a
WilsonSinclair <wilsonsinclair6@gmail.com>
2022-10-10 23:00:23 -0500

    small changes to test code

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -7,14 +7,14 @@ public class PlayerTest {
     @Test
     public void hasPlayableCardWhenFirstCardIsDrawFourTest() {
         Game game = new Game(1);
-        game.setCurrentColor(Colors.COLORLESS);
-        assertTrue(game.currentPlayer.hasPlayableCard(new WildCard(WildCardType.DrawFour), game));
+        Game.setCurrentColor(Colors.COLORLESS);
+        assertTrue(Game.currentPlayer.hasPlayableCard(new WildCard(WildCardType.DrawFour), game));
     }
 
     @Test
     public void hasPlayableCardWhenFirstCardIsWildCardTest() {
         Game game = new Game(1);
-        game.setCurrentColor(Colors.COLORLESS);
-        assertTrue(game.currentPlayer.hasPlayableCard(new WildCard(WildCardType.Wild), game));
+        Game.setCurrentColor(Colors.COLORLESS);
+        assertTrue(Game.currentPlayer.hasPlayableCard(new WildCard(WildCardType.Wild), game));
     }
 }
