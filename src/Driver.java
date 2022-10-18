a3d7ebc89b14031913be319b47cac59915f7cea6
Wilson <wilsonsinclair6@gmail.com>
2022-10-17 23:38:01 -0500

    Added lib dir for testing in vscode

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,5 @@ public class Driver {
     public static void main(String[] args) {
         System.out.print("Number of Players: ");
         new Game(new Scanner(System.in).nextInt()).play();
-
     }
 }
